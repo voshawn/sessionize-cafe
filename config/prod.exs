d6ed1a63fa1ee4ee7964453866551804f4c25ef9
Shawn Vo <shawn.vo@gmail.com>
2018-12-02 15:57:34 -0500

    updated domain

--- config/prod.exs
+++ config/prod.exs
@@ -11,9 +11,9 @@ use Mix.Config
 # before starting your production server.
 config :cafe, CafeWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "arcane-cliffs-33953.herokuapp.com", port: 443],
+  # url: [scheme: "https", host: "arcane-cliffs-33953.herokuapp.com", port: 443],
   # For a custom domain
-  # url: [scheme: "https", host: "mywebsite.com", port: System.get_env("PORT")],
+  url: [scheme: "https", host: "sessionize.me", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
